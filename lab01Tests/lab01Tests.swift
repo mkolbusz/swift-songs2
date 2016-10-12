07346f5e4b7f295e36dab8d2858943f926f820a5
UŇľytkownik GoŇõńá <Guest@imac-11.local>
2016-10-12 16:45:43 +0200

    Initial Commit

---
+++ lab01Tests/lab01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  lab01Tests.swift
+//  lab01Tests
+//
+//  Created by Użytkownik Gość on 12.10.2016.
+//  Copyright © 2016 Użytkownik Gość. All rights reserved.
+//
+
+import XCTest
+@testable import lab01
+
+class lab01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
